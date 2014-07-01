c3d952fc7fbbdd5b7fa455c5339bcf707da18fe6
antoineludwig <antoine.ludwig60@gmail.com>
2014-07-01 20:51:53 +0200

    Ajout de LancerSimulation et des attriburs à Simulation

--- simulation.cpp
+++ simulation.cpp
@@ -3,3 +3,18 @@
 Simulation::Simulation()
 {
 }
+
+/**
+ * @brief Simulation::LancerSimulation
+ * Lancement de la simulation
+ */
+void Simulation::LancerSimulation(){
+
+    //parcours de la liste des tâches
+    //for chaque tache
+        //effectue la tache (déplacement ou rangement)
+        //tu peux utiliser les fonctions dans robot pour les déplacer
+        //tu peux accéder au tableau par e.tab[][] (il est en public)
+        //les constantes LONGEUR et LARGEUR définisse la taille max du tableau
+    //fin for
+}
